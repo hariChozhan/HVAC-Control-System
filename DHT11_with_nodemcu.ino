a0571ee55f943ab60be4fadee33bec5f99cd3e2a
Hari Prasanth Chozhan <24803379+Harichozhan@users.noreply.github.com>
2018-10-29 15:58:58 +0530

    Add files via upload

---
+++ DHT11_with_nodemcu.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h>
+#include <ThingerESP8266.h>
+
+#define USERNAME "HARICHOZHAN"
+#define DEVICE_ID "dht11"
+#define DEVICE_CREDENTIAL "************" // device credential detail
+
+#define SSID "dj"
+#define SSID_PASSWORD "0987654321"
+
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+#include "DHT.h"        
+#define DHTTYPE DHT11  
+
+#define dht_dpin 0
+DHT dht(dht_dpin, DHTTYPE); 
+void setup(void)
+{ 
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humidity and temperature\n\n");
+  delay(1000);
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();         
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.println("C  ");
+  delay(1000);
+  thing.add_wifi(SSID, SSID_PASSWORD);
+  thing["temperature"] >> outputValue(dht.readTemperature());
+  thing["humidity"] >> outputValue(dht.readHumidity());
+}
+void loop() {
+  thing.handle();  
+}
---
+++ hvac.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+float tempC;
+int reading;
+int tempPin = 0;
+int led =13;
+#define CW 7
+#define CCW 8
+void setup(){
+  pinMode(led,OUTPUT);
+  pinMode(CW,OUTPUT);
+  pinMode(CCW, OUTPUT);
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+reading = analogRead(tempPin);
+tempC = (5.0*reading*100.0)/1024.0;
+Serial.print("temp in C =");
+Serial.println(tempC);
+if (tempC <  32){
+  digitalWrite(led,HIGH);
+  digitalWrite(CW,HIGH);
+  delay(1000);
+  digitalWrite(CW,LOW);
+}
+else if (tempC > 32){
+  digitalWrite(led,LOW);
+  digitalWrite(CCW,HIGH);
+  delay(1000);
+  digitalWrite(CCW,LOW);
+  }
+}
